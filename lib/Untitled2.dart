3e0df71efd0e532a63e2c74a55f044ae46ec885f
susumOyaji <sumitomo0210@gmail.com>
2023-07-29 08:45:03 +0900

    up

--- lib/Untitled2.dart
+++ lib/Untitled2.dart
@@ -1,4 +1,5 @@
 // main.dart
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
---
+++ lib/float.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          floatingActionButton: Column(
+            verticalDirection: VerticalDirection.up, // childrenの先頭を下に配置
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FloatingActionButton(
+                backgroundColor: Colors.redAccent,
+                onPressed: () {
+                  print("pressed");
+                },
+                mini: true, // 小さいサイズのFloatingActionButtonを表示
+              ),
+              Container(
+                // 余白のためContainerでラップ
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: FloatingActionButton(
+                  backgroundColor: Colors.amberAccent,
+                  onPressed: () {
+                    print("pressed");
+                  },
+                  mini: true, // 小さいサイズのFloatingActionButtonを表示
+                ),
+              ),
+              Container(
+                // 余白のためContainerでラップ
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: FloatingActionButton(
+                  backgroundColor: Colors.amberAccent,
+                  onPressed: () {
+                    print("pressed");
+                  },
+                  mini: true, // 小さいサイズのFloatingActionButtonを表示
+                ),
+              ),
+              Container(
+                // 余白のためContainerでラップ
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: FloatingActionButton(
+                  backgroundColor: Colors.amberAccent,
+                  onPressed: () {
+                    print("pressed");
+                  },
+                  mini: true, // 小さいサイズのFloatingActionButtonを表示
+                ),
+              ),
+            ],
+          ),
+          appBar: AppBar(
+            title: Text("Hoge"),
+          ),
+          body: Text("Hoge")),
+    );
+  }
+}
