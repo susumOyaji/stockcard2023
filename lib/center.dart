25732d9933aa9dd8c253b5dea488503e4e25cbfb
susumOyaji <sumitomo0210@gmail.com>
2024-02-08 17:43:10 +0900

    up

---
+++ lib/center.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Center Widget Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Center Widget Example'),
+        ),
+        body: const SafeArea(
+          child: Center(
+            // Centerウィジェットが親ウィジェットの中央に配置される
+            child: Column(
+              //Columnの場合は垂直方向は中央に配置されるわけではありません。
+              //全てのウィジットが中央に重なってしまうので配置位置を明示的に指定する必要があります。
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              Text(
+                '1st.Hello, World!',
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                '2nd.Hello, World!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
